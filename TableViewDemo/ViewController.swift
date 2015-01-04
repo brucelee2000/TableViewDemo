77298fc310d1c5f647666571f1e886e7a6fcf232
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-03 18:41:32 -0800

    Setup TableView datasource from an array.

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
+    
+    // Define some tuples as an array
+    let devCourses = [("iOS Book 1", "John A"),
+                      ("iOS Book 2", "John B"),
+                      ("iOS Book 3", "John C"),
+                      ("iOS DVD A", "Doe 1"),
+                      ("iOS DVD B", "Doe 2"),
+                      ("iOS DVD C", "Doe 3"),
+                      ("iOS Song X1", "Alice AA"),
+                      ("iOS Song X2", "Alice BB"),
+                      ("iOS Song X3", "Alice CC")]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +41,13 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return devCourses.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let (courseTitle, courseAuthor) = devCourses[indexPath.row]
+        cell.textLabel?.text = courseTitle
         
         return cell
     }
